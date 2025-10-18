62b39ceddf1f97d91c4024e53b631459ad1f8132
Izuchukwu Oruche <izuchukwuoruche@gmail.com>
2025-10-18 21:46:53 +0100

    Edited config.py file by adding lines for the dynamodb database

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ PHONE_NUMBER_ID   = os.environ["PHONE_NUMBER_ID"]
 WHATSAPP_TOKEN    = os.environ["WHATSAPP_TOKEN"]
 VERIFY_TOKEN      = os.environ["VERIFY_TOKEN"]
 DEFAULT_FALLBACK_LANG = os.getenv("DEFAULT_FALLBACK_LANG", "en")
+DDB_TABLE = os.environ["DDB_TABLE"]
 
 # AWS clients (use Lambda's region automatically)
 comprehend = boto3.client("comprehend")
 translate  = boto3.client("translate")
+ddb = boto3.resource("dynamodb").Table(DDB_TABLE)
 
 # Basic language constants
 SUPPORTED_TRANSLATE_BIDI = {"ha"}  # two-way translation en<->ha
