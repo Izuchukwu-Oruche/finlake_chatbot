c7c3af1f9f2af6bad50f5cf1fa3a5066501fb1d2
Izuchukwu Oruche <izuchukwuoruche@gmail.com>
2025-10-19 03:20:10 +0100

    Added an import statement in the http_utils.py file

--- http_utils.py
+++ http_utils.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def ok(body="OK", status=200, content_type="text/plain"):
     return {"statusCode": status, "headers": {"Content-Type": content_type}, "body": body}
 
